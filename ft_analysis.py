eac4c3e887516e0052d9970055963b187eda8f92
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-10-22 12:42:24 +0300

    Update ft_analysis.py

--- ft_analysis.py
+++ ft_analysis.py
@@ -1,50 +1,40 @@
-def ft_len(string):
-    count = 0
-    for _ in string:
-        count += 1
-    return count
+def ft_len(a):
+    c = 0
+    for i in a:
+        c += 1
+    return (c)
 
 
-def ft_slice_str(string, start, stop):
-    result_string = ''
-    for i in range(start, stop):
-        result_string += string[i]
-    return result_string
-
-
-def ft_even_place(string):
-    result_string = str()
-    for i in range(ft_len(string)):
-        if i % 2 == 1:
-            result_string += string[i]
-    return result_string
-
-
-def ft_n_even_place(string):
-    result_string = str()
-    for i in range(ft_len(string)):
+def ft_analysis(a):
+    print(a[2])
+    print(a[ft_len(a) - 2])
+    print(a[0], a[1], a[2], a[3], a[4], sep="")
+    i = 0
+    while i != ft_len(a) - 2:
+        print(a[i], end="")
+        i += 1
+    i = 0
+    print(end="\n")
+    while i != ft_len(a):
         if i % 2 == 0:
-            result_string += string[i]
-    return result_string
-
-
-def ft_reverse_str(string):
-    result_string = ''
-    for i in range(-1, -ft_len(string) - 1, -1):
-        result_string += string[i]
-    return result_string
-
-
-def ft_analysis(string):
-    result = ''
-    print(string[2])
-    print(string[-2])
-    print(ft_slice_str(string, 0, 5))
-    print(ft_slice_str(string, 0, ft_len(string) - 2))
-    print(ft_even_place(string))
-    print(ft_n_even_place(string))
-    print(ft_slice_str(string, ft_len(string), 0))
-    for i in range(-1, -ft_len(string) - 1, -2):
-        result += string[i]
-    print(result)
-    print(ft_len(string))
+            print(a[i], end="")
+        i += 1
+    print(end="\n")
+    i = 0
+    while i != ft_len(a):
+        if i % 2 != 0:
+            print(a[i], end="")
+        i += 1
+    print(end="\n")
+    i = 0
+    while i != ft_len(a):
+        print(a[ft_len(a) - i - 1], end="")
+        i += 1
+    print(end='\n')
+    i = 0
+    while i != ft_len(a):
+        if i % 2 == 0:
+            print(a[ft_len(a) - i - 1], end="")
+        i += 1
+    print(end='\n')
+    print(ft_len(a))

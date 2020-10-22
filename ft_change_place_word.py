688d07d334a294b644286175ec5592a28c4703bd
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-10-22 12:45:28 +0300

    Update ft_change_place_word.py

--- ft_change_place_word.py
+++ ft_change_place_word.py
@@ -1,3 +1,21 @@
-def ft_change_place_word(string):
-    string = string.split()
-    return string[-1] + ' ' + string[0]
+def ft_len(a):
+    d = 0
+    for i in a:
+        d += 1
+    return d
+
+
+def ft_change_place_word(a):
+    i = 0
+    s1 = ''
+    s2 = ''
+    s3 = ''
+    while a[i] != ' ':
+        s1 += a[i]
+        i += 1
+    i += 1
+    while i < ft_len(a):
+        s2 += a[i]
+        i += 1
+        s3 = s2 + ' ' + s1
+    return s3
